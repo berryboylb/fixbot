c79d53d4b2a794b781283b800b3c9ad95c619bbb
berryboylb <phemmynesce4life@gmail.com>
2021-09-12 03:17:59 +0100

    Added basic boiler plate

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src="/work.jpg" alt="image" />
+      <img src="/work.jpg" alt="progress" />
         <h1>Fixbot landing Page</h1>
     </div>
   );
