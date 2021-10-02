3fda5a5a4c2d21759709690edd9b8cc46f489b77
berryboylb <phemmynesce4life@gmail.com>
2021-10-02 01:33:14 +0100

    fixed products menu

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,29 +1,27 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 import H1 from "./H1"
 import H3 from "./H3"
 const Product = ({products}) => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+    // const responsive = {
+    //     superLargeDesktop: {
+    //       // the naming can be any, depends on you.
+    //       breakpoint: { max: 4000, min: 3000 },
+    //       items: 5
+    //     },
+    //     desktop: {
+    //       breakpoint: { max: 3000, min: 1024 },
+    //       items: 3
+    //     },
+    //     tablet: {
+    //       breakpoint: { max: 1024, min: 464 },
+    //       items: 2
+    //     },
+    //     mobile: {
+    //       breakpoint: { max: 464, min: 0 },
+    //       items: 1
+    //     }
+    //   };
     return (
         <>
         <div className="product">
